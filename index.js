5768c6fc30f989fcec3760ae3a4858315a949b7c
frimpong1781 <frimpong1781@yahoo.com>
2021-07-10 09:31:08 +0100

    added body-parser definition

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const server = express('server');
 
+// const loginRequestHandler = (req, res) => {
+//      //  let body = '';
+//      //  req.on('data', (chunk) => {
+//      //       body += chunk;
+//      //  });
+
+//      //  req.on('end', () => {
+//      //       console.log(body);
+//      //  });
+//      console.log(req.body.email);
+//      console.log(req.body.password);
+//      res.send('Done');
+// };
+
+const signupRequestHandler = (req, res) => {
+     console.log(req.body.fullname);
+     console.log(req.body.email);
+     console.log(req.body.password);
+     console.log(req.body.confirmpassword);
+     res.send('Good');
+};
+
 //middleware definitions
 server.use(express.static(path.join(__dirname, 'public')));
+server.use(bodyParser.urlencoded({ extended: false }));
 
-// const serveHomePage = (req, res) => {
-//      const homepageFilePath = path.join(__dirname, 'public', 'index.html');
-//      res.sendFile(homepageFilePath);
-// };
-
-// const serveProfilePage = (req, res) => {
-//      const profilePageFilePath = path.join(__dirname, 'public', 'profile.html');
-//      res.sendFile(profilePageFilePath);
-// };
-// const serveAboutPage = (req, res) => {
-//      const aboutPageFilePath = path.join(__dirname, 'public', 'about.html');
-//      res.sendFile(aboutPageFilePath);
-// };
-// const servePortfolioPage = (req, res) => {
-//      const portfolioPageFilePath = path.join(
-//           __dirname,
-//           'public',
-//           'portfolio.html'
-//      );
-//      res.sendFile(portfolioPageFilePath);
-// };
-
-////routes definitions
-// server.get('/', serveHomePage);
-// server.get('/profile', serveProfilePage);
-// server.get('/about', serveAboutPage);
-// server.get('/portfolio', servePortfolioPage);
+//route
+// server.post('/login', loginRequestHandler);
+server.post('/signup', signupRequestHandler);
 
 server.listen(3000, () => console.log('server is ready'));
