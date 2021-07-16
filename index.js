d21305cb1b86fcba1bcdce0093a03c372bf626c7
frimpong1781 <frimpong1781@yahoo.com>
2021-07-16 19:25:05 +0100

    few changes

--- index.js
+++ index.js
@@ -4,26 +4,17 @@ const bodyParser = require('body-parser');
 
 const server = express('server');
 
-// const loginRequestHandler = (req, res) => {
-//      //  let body = '';
-//      //  req.on('data', (chunk) => {
-//      //       body += chunk;
-//      //  });
-
-//      //  req.on('end', () => {
-//      //       console.log(body);
-//      //  });
-//      console.log(req.body.email);
-//      console.log(req.body.password);
-//      res.send('Done');
-// };
-
+const loginRequestHandler = (req, res) => {
+     console.log(req.body.email);
+     console.log(req.body.password);
+     res.send('Done');
+};
 const signupRequestHandler = (req, res) => {
      console.log(req.body.fullname);
      console.log(req.body.email);
      console.log(req.body.password);
      console.log(req.body.confirmpassword);
-     res.send('Good');
+     res.send('Signup Successfully');
 };
 
 //middleware definitions
@@ -31,7 +22,7 @@ server.use(express.static(path.join(__dirname, 'public')));
 server.use(bodyParser.urlencoded({ extended: false }));
 
 //route
-// server.post('/login', loginRequestHandler);
+server.post('/login', loginRequestHandler);
 server.post('/signup', signupRequestHandler);
 
 server.listen(3000, () => console.log('server is ready'));
